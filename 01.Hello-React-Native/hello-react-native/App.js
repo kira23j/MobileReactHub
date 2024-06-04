b9667107f0f92495c9c86ae0751282ac3bfdb67b
kira23j <kirubelmamuyesahle@gmail.com>
2024-06-04 08:32:24 -0700

    updating the repo and adding fundamentals on hello-react-native

--- 01.Hello-React-Native/hello-react-native/App.js
+++ 01.Hello-React-Native/hello-react-native/App.js
@@ -1,26 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableNativeFeedback,View,Image, SafeAreaView,Button, Alert } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+  <SafeAreaView style={[styles.container,anotherStyle]}>
+<Text>Hello React-Native</Text>
+<Text style={styles.Text} >Welcome</Text>
+<TouchableNativeFeedback onPress={()=>console.log("touched")}>
+<Image 
+source={require('./assets/favicon.png')}/>
+</TouchableNativeFeedback>
+<Button color="cyan" title='click' onPress={()=>Alert.alert("Hello React-Native","verify your gender",[{text:"male",onPress:()=>alert("yap you are a man")},
+  {text:"female", onPress:()=>alert("yap you are a lady")}
+])}/>
+<Button color="orange" title='another one' onPress={()=>Alert.prompt("ur name please","another one")} />
+  </SafeAreaView>
+     
     
-      <Text>Hello-React-Native</Text>
-      <Text>This is first working react native app</Text>
-      <Text>that is it bro well done</Text>
-      <Text>that is it bro well done</Text>
-      <Text>that is it bro well done</Text>
-      <Text>that is it bro well done</Text>
-      <StatusBar style="auto" />
-    </View>
+
   );
 }
+const anotherStyle={backgroundColor:"grey"}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'cyan',
+    backgroundColor: 'aquamarine',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Text:{
+color:'red',
+fontStyle:"italic",
+
   },
 });
